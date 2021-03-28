118239819219572260af5121f6341dcc98607965
alexfeds <alexfeds@gmail.com>
2021-03-29 00:17:10 +0100

    init commit

---
+++ Controllers/SaleController.cs
@@ -0,0 +1,20 @@
+﻿using grid_evolve.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace grid_evolve.Controllers
+{
+    public class SaleController : Controller
+    {
+
+        public static List<SaleData> sale = SaleData.GetData(15).ToList();
+        // GET: Sale
+        public ActionResult Sale()
+        {
+            return View(sale);
+        }
+    }
+}
